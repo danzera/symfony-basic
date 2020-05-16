9359235377ef960549c17407a8b813c6d1a8d472
Dan Zera <dczera@gmail.com>
2020-05-16 12:16:26 -0700

    include a template for the /lucky/number route instead of returning HTML directly from the controller function

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController {
+// need to extend AbstractController to use Twig templating
+class LuckyController extends AbstractController {
 	// generate a random integer in the range 0-100
 	public function number() {
 		$number = random_int(0, 100);
 
-		return new Response(
-			'<html><body>Lucky number: '.$number.'</body></html>'
-		);
+		// returning HTML without using templating
+		// return new Response(
+			// '<html><body>Lucky number: '.$number.'</body></html>'
+		// );
+
+		// returning HTML with Twig templating
+		// Twigs render function takes a file name, relative to the templates/ directory
+		// it also takes an associative array for injecting variables into the template
+		return $this->render('lucky/number.html.twig', [
+			'number' => $number
+		]);
 	}
 
 }
